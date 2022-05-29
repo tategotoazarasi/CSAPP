3765a82eb21077206d384e9d0008c3ae2bbdf7e5
Tategoto Azarasi <2724167997@qq.com>
2022-05-29 21:42:20 +0800

    Optimizing Matrix Transpose
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -20,7 +20,79 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {}
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+	int i, j, tmp;
+	int n = 8;
+	if(M == 32 || M == 61) {
+		if(M == 61) {
+			n = 16;
+		}
+		for(i = 0; i < N; i += n) {
+			for(j = 0; j < M; j += n) {
+				for(int ii = 0; ii < n && i + ii < N; ii++) {
+					for(int jj = 0; jj < n && j + jj < M; jj++) {
+						tmp               = A[i + ii][j + jj];
+						B[j + jj][i + ii] = tmp;
+					}
+				}
+			}
+		}
+	} else if(M == 64) {
+		int i, j, x, y;
+		int x1, x2, x3, x4, x5, x6, x7, x8;
+		for(i = 0; i < N; i += n)
+			for(j = 0; j < M; j += n) {
+				for(x = i; x < i + 4; ++x) {
+					x1 = A[x][j];
+					x2 = A[x][j + 1];
+					x3 = A[x][j + 2];
+					x4 = A[x][j + 3];
+					x5 = A[x][j + 4];
+					x6 = A[x][j + 5];
+					x7 = A[x][j + 6];
+					x8 = A[x][j + 7];
+
+					B[j][x]         = x1;
+					B[j + 1][x]     = x2;
+					B[j + 2][x]     = x3;
+					B[j + 3][x]     = x4;
+					B[j][x + 4]     = x5;
+					B[j + 1][x + 4] = x6;
+					B[j + 2][x + 4] = x7;
+					B[j + 3][x + 4] = x8;
+				}
+				for(y = j; y < j + 4; ++y) {
+					x1 = A[i + 4][y];
+					x2 = A[i + 5][y];
+					x3 = A[i + 6][y];
+					x4 = A[i + 7][y];
+					x5 = B[y][i + 4];
+					x6 = B[y][i + 5];
+					x7 = B[y][i + 6];
+					x8 = B[y][i + 7];
+
+					B[y][i + 4]     = x1;
+					B[y][i + 5]     = x2;
+					B[y][i + 6]     = x3;
+					B[y][i + 7]     = x4;
+					B[y + 4][i]     = x5;
+					B[y + 4][i + 1] = x6;
+					B[y + 4][i + 2] = x7;
+					B[y + 4][i + 3] = x8;
+				}
+				for(x = i + 4; x < i + 8; ++x) {
+					x1          = A[x][j + 4];
+					x2          = A[x][j + 5];
+					x3          = A[x][j + 6];
+					x4          = A[x][j + 7];
+					B[j + 4][x] = x1;
+					B[j + 5][x] = x2;
+					B[j + 6][x] = x3;
+					B[j + 7][x] = x4;
+				}
+			}
+	}
+}
 
 /*
  * You can define additional transpose functions below. We've defined
